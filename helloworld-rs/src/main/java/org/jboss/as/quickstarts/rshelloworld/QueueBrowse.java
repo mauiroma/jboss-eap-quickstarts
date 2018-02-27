ea0a39e16ce69ce1105f694b4414d662323e2b84
UTENZE\wsvi168 <MAURIZIO.ROMANI@ESTERNI.BANCADITALIA.IT>
2018-02-27 13:36:54 +0100

    Modified json output

--- helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/QueueBrowse.java
+++ helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/QueueBrowse.java
@@ -5,12 +5,8 @@ package org.jboss.as.quickstarts.rshelloworld;
 import org.json.simple.JSONObject;
 import java.util.Date;
 import java.util.Enumeration;
-import java.util.Hashtable;
-import javax.annotation.Resource;
 import javax.jms.*;
-import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,19 +20,19 @@ public class QueueBrowse
     private Session qsession;
     private QueueBrowser qbrowser;
 
-    @Resource(lookup = "/jms/riadCF")
-    ConnectionFactory qconFactory;
 
 
     @GET
     @Path("/browse")
     @Produces({ "application/json" })
-    public String displayQueue(@QueryParam("queue") String queueJndiName, @QueryParam("print") boolean printMessage) throws JMSException {
+    public String displayQueue(@QueryParam("cf") String connectionFactoryJndiName, @QueryParam("queue") String queueJndiName, @QueryParam("print") boolean printMessage) throws JMSException {
         JSONObject response = new JSONObject();
         try {
+            InitialContext context = new InitialContext();
+            ConnectionFactory qconFactory = (ConnectionFactory) context.lookup(connectionFactoryJndiName);
             qcon = qconFactory.createConnection();
             qsession = qcon.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = (Queue) new InitialContext().lookup(queueJndiName);
+            Queue queue = (Queue) context.lookup(queueJndiName);
             qbrowser = qsession.createBrowser(queue);
             qcon.start();
 
@@ -45,7 +41,9 @@ public class QueueBrowse
                 response.put("Result", "No message on ["+queue.getQueueName()+"]");
             } else {
                 JSONObject listMessages = new JSONObject();
+                int messages = 0;
                 while (e.hasMoreElements()) {
+                    messages++;
                     JSONObject message = new JSONObject();
                     Message m = (Message) e.nextElement();
 
@@ -55,30 +53,27 @@ public class QueueBrowse
                         String propValue = m.getStringProperty(propName);
                         message.put(propName,propValue);
                     }
-
-                    message.put("Delivered",new Date(m.getJMSTimestamp()).toString());
-
-                    message.put("Delivered Oracle", new Date(m.getLongProperty("JMS_BEA_DeliveryTime")).toString());
-
+                    message.put("JMSTimestamp(Delivered)",new Date(m.getJMSTimestamp()).toString());
                     if (m.getJMSExpiration() > 0) {
-                        message.put("Expires",new Date( m.getJMSExpiration()));
+                        message.put("JMSExpiration",new Date( m.getJMSExpiration()));
                     }else {
-                        message.put("Expires","never");
+                        message.put("JMSExpiration","never");
                     }
 
-                    message.put("Priority",m.getJMSPriority());
-                    message.put("Mode",(m.getJMSDeliveryMode() == DeliveryMode.PERSISTENT ?"PERSISTENT" : "NON_PERSISTENT"));
-                    message.put("Correlation ID",m.getJMSCorrelationID());
-                    message.put("Reply to",m.getJMSReplyTo());
-                    message.put("Message type",m.getJMSType());
+                    message.put("JMSPriority",m.getJMSPriority());
+                    message.put("JMSDeliveryMode",(m.getJMSDeliveryMode() == DeliveryMode.PERSISTENT ?"PERSISTENT" : "NON_PERSISTENT"));
+                    message.put("JMSCorrelationID",m.getJMSCorrelationID());
+                    message.put("JMSReplyTo",m.getJMSReplyTo());
+                    message.put("JMSType",m.getJMSType());
                     if (printMessage && m instanceof TextMessage) {
                         message.put("TextMessage",((TextMessage)m).getText() );
                     }
                     listMessages.put("Message "+m.getJMSMessageID(),message);
                 }
-                response.put(queue.getQueueName(),listMessages);
+                response.put(queue.getQueueName() +" #["+messages+"]",listMessages);
             }
         } catch (Exception e1) {
+            e1.printStackTrace();
             response.put("Exception", e1);
         } finally {
             if(qbrowser!=null){
@@ -91,7 +86,7 @@ public class QueueBrowse
                 qcon.close();
             }
         }
-        return response.toJSONString();
+        return response.toString();
     }
 
 
